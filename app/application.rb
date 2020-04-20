a6ea8094980bf0d1dc429279bde55e476f217dc7
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-20 12:55:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,13 +28,12 @@ class Application
 #Otherwise give an error
     elsif req.path.match(/add/)
       add_item = req.params["item"]
-      if @@items.include? add_item
-        @@cart << add_item
-        resp.write "added #{add_item}"
+      handle_search(search_term) == "#{search_term} is one of our items"
+        @@cart << search_term
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
-
     else
       resp.write "Path Not Found"
     end
