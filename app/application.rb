15ea6abb6d0f5480972874093f763dac5a8b46c8
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-20 12:50:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,6 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-  #Create a new class array called @@cart to hold any items in your cart
-  @@cart=[]
 
   def call(env)
     resp = Rack::Response.new
@@ -12,24 +10,29 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      #Create a new route called /cart to show the items in your cart
-    elsif req.path.match(/add/)
-        search_term = req.params["item"]
-        if handle_search(search_term) == "#{search_term} is one of our items"
-          @@cart << search_term
-          resp.write "added #{search_term}"
-        else
-          resp.write "We don't have that item"
-        end
+
     elsif req.path.match(/cart/)
-   if @@cart.length > 0
-     @@cart.each { |item| resp.write "#{item}\n" }
-   else
-     resp.write "Your cart is empty"
-   end
+      if @@cart.length == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/)
+      add_item = req.params["item"]
+      if @@items.include? add_item
+        @@cart << add_item
+        resp.write "added #{add_item}"
+      else
+        resp.write "We don't have that item"
+      end
+
     else
       resp.write "Path Not Found"
     end
